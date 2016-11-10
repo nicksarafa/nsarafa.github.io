1a9121f3cf56c0fdb62f0fcc89051acf4f97378d
nsarafa <nicksarafa@gmail.com>
2016-11-10 13:51:46 +0700

    gulp buildJs, buildCss, cleanJs, cleanCss tasks init

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,3 @@
-import 'babel-polyfill'
-
 import gulp from 'gulp'
 import babel from 'gulp-babel'
 import sass from 'gulp-sass'
@@ -7,26 +5,31 @@ import sourceMaps from 'gulp-sourcemaps'
 import del from 'del'
 
 const paths = {
-  allSrcJs: 'src/js/*.js',
+  srcDir: 'src',
   allSrcCss: 'src/css/*.css',
+  allSrcJs: 'src/js/*.js',
   distDir: 'docs',
+  allDistCss: 'docs/css/*.css',
+  allDistJs: 'docs/js/*.js',
   gulpFile: 'gulpfile.babel.js',
 }
 
 gulp.task('clean', () => del(paths.distDir))
+gulp.task('cleanCss', () => del(paths.allDistCss))
+gulp.task('cleanJs', () => del(paths.allDistJs))
 
 gulp.task('watch', () => {
   gulp.watch(paths.allSrcCss, ['css'])
   gulp.watch(paths.allSrcJs, ['js'])
 })
 
-gulp.task('js', ['clean'], () => {
+gulp.task('js', ['cleanJs'], () => {
   return gulp.src(paths.allSrcJs)
     .pipe(babel())
     .pipe(gulp.dest(paths.distDir))
 })
 
-gulp.task('css', ['clean'], () => {
+gulp.task('css', ['cleanCss'], () => {
   return gulp.src(paths.allSrcCss)
     .pipe(sass())
     .pipe(gulp.dest(paths.distDir))
