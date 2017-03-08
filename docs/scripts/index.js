331ca5a4b4350b6a620c65cdc0e9ae8aab656df5
nsarafa <nicksarafa@gmail.com>
2017-03-08 14:28:19 +0800

    core menu open/close js init

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -3,9 +3,19 @@
 // scripts here
 
 window.onload = function () {
+  var isMenuOpen = false;
+  var body = document.getElementById('body');
   var menuButton = document.getElementById('menuButton');
 
   menuButton.onclick = function (event) {
-    alert('button clicked');
+    event.preventDefault(event);
+
+    if (isMenuOpen === false) {
+      body.style.paddingRight = '330px';
+      isMenuOpen = true;
+    } else if (isMenuOpen === true) {
+      body.style.paddingRight = '0px';
+      isMenuOpen = false;
+    }
   };
 };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,19 @@
 // scripts here
 
 window.onload = function () {
+  let isMenuOpen = false
+  const body = document.getElementById('body')
   const menuButton = document.getElementById('menuButton')
 
-  menuButton.onclick = function(event) { alert('button clicked') }
+  menuButton.onclick = function(event) {
+    event.preventDefault(event)
+
+    if(isMenuOpen === false) {
+      body.style.paddingRight = '330px'
+      isMenuOpen = true
+    } else if (isMenuOpen === true) {
+      body.style.paddingRight = '0px'
+      isMenuOpen = false
+    }
+  }
 }
